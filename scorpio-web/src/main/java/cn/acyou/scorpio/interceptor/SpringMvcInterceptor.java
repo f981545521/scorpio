a0b1558711e1320b638edcf7248a24a3bb4d3e31
youfang <981545521@qq.com>
2020-07-14 15:30:19 +0800

    获取用户User-agent

--- scorpio-web/src/main/java/cn/acyou/scorpio/interceptor/SpringMvcInterceptor.java
+++ scorpio-web/src/main/java/cn/acyou/scorpio/interceptor/SpringMvcInterceptor.java
@@ -1,5 +1,6 @@
 package cn.acyou.scorpio.interceptor;
 
+import cn.acyou.framework.utils.UserAgentUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
@@ -21,6 +22,13 @@ public class SpringMvcInterceptor extends HandlerInterceptorAdapter {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         log.info("———————————————————————————— SpringMvcInterceptor start ————————————————————————————");
+        String ua = request.getHeader("User-Agent");
+        log.info("———————————————————————————— SpringMvcInterceptor 获取到User-Agent : "+ua+" ————————————————————————————");
+
+        boolean weChat = UserAgentUtil.isWeChat(request);
+        boolean ie = UserAgentUtil.isIEBrowser(request);
+        boolean mb = UserAgentUtil.isMobile(request);
+
         return super.preHandle(request, response, handler);
     }
 
