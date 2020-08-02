70f97130c8967cc6c4e750a4cd9c9177ea4fc9a0
youfang <981545521@qq.com>
2020-08-02 22:32:22 +0800

    多线程与线程池

--- scorpio-web/src/main/java/cn/acyou/scorpio/conf/ThreadPoolConfig.java
+++ scorpio-web/src/main/java/cn/acyou/scorpio/conf/ThreadPoolConfig.java
@@ -23,20 +23,26 @@ import java.util.concurrent.ThreadPoolExecutor;
 public class ThreadPoolConfig extends AsyncConfigurerSupport {
 
     @Bean
-    public ThreadPoolTaskExecutor ibExecutor() {
+    public ThreadPoolTaskExecutor poolExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setThreadNamePrefix("IBlog-Executor-");
+        //线程名称的前缀
+        executor.setThreadNamePrefix("ThreadPool-Executor-");
+        //核心线程池数量
         executor.setCorePoolSize(10);
+        //最大线程数量
         executor.setMaxPoolSize(50);
+        //线程池的队列容量
         executor.setQueueCapacity(100);
+        //线程存活时间（秒）
         executor.setKeepAliveSeconds(300);
+        //setRejectedExecutionHandler：当pool已经达到max size的时候，如何处理新任务 - CallerRunsPolicy：不在新线程中执行任务，而是由调用者所在的线程来执行
         executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
         return executor;
     }
 
     @Override
     public Executor getAsyncExecutor() {
-        return ibExecutor();
+        return poolExecutor();
     }
 
     @Override
---
+++ scorpio-web/src/test/java/cn/acyou/example/ThreadAndPoolTest.java
@@ -0,0 +1,104 @@
+package cn.acyou.example;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
+/**
+ * @author youfang
+ * @version [1.0.0, 2020-8-2 下午 07:16]
+ **/
+public class ThreadAndPoolTest {
+
+    @Test
+    public void test1(){
+        ExecutorService cachedThreadPool = Executors.newCachedThreadPool();
+        ExecutorService fixedThreadPool = Executors.newFixedThreadPool(10);
+        ExecutorService singleThreadExecutor = Executors.newSingleThreadExecutor();
+        ExecutorService scheduledExecutor = Executors.newScheduledThreadPool(10);
+        ExecutorService workStealingPool = Executors.newWorkStealingPool(10);
+
+    }
+
+    @Test
+    public void testThreadPool(){
+        //ExecutorService fixedThreadPool = Executors.newFixedThreadPool(2);
+        //队列大小默认是: Integer.MAX_VALUE
+        System.out.println(Integer.MAX_VALUE);
+
+
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(5, 5,
+                0L, TimeUnit.MILLISECONDS,
+                new LinkedBlockingQueue<Runnable>());
+    }
+    @Test
+    public void testLinkedBlockingDeque(){
+        //阻塞队列 add满了后会抛异常
+        LinkedBlockingDeque<String> objects = new LinkedBlockingDeque<>(10);
+
+        for (int i = 0; i < 1000; i++) {
+            objects.add("ok" + i);
+        }
+        //take
+        //put
+        System.out.println("ok");
+        //Exception in thread "main" java.lang.IllegalStateException: Deque full
+        //	at java.util.concurrent.LinkedBlockingDeque.addLast(LinkedBlockingDeque.java:335)
+        //	at java.util.concurrent.LinkedBlockingDeque.add(LinkedBlockingDeque.java:633)
+        //	at cn.acyou.example.ThreadAndPoolTest.main(ThreadAndPoolTest.java:40)
+    }
+
+    @Test
+    public void testDelayDeque(){
+        //阻塞队列 add满了后会抛异常
+        LinkedBlockingDeque<String> objects = new LinkedBlockingDeque<>(10);
+
+        for (int i = 0; i < 1000; i++) {
+            objects.add("ok" + i);
+        }
+        //take
+        //put
+        System.out.println("ok");
+        //Exception in thread "main" java.lang.IllegalStateException: Deque full
+        //	at java.util.concurrent.LinkedBlockingDeque.addLast(LinkedBlockingDeque.java:335)
+        //	at java.util.concurrent.LinkedBlockingDeque.add(LinkedBlockingDeque.java:633)
+        //	at cn.acyou.example.ThreadAndPoolTest.main(ThreadAndPoolTest.java:40)
+    }
+
+    @Test
+    public void testListMaxSize(){
+        //疑问：List最大长度是多大，TODO : 去公司执行一下试试。
+        List<String> objects = new ArrayList<>(10);
+        long i = 0;
+        while (i < Long.MAX_VALUE) {
+            objects.add("ok" + i++);
+        }
+        System.out.println("ok");
+    }
+
+    public static void main(String[] args) {
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(5, 5,
+                0L, TimeUnit.MILLISECONDS,
+                new LinkedBlockingQueue<>(10));
+        for (int i = 0; i < 100; i++) {
+            threadPoolExecutor.execute(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println("执行中...");
+                    System.out.println(threadPoolExecutor.getActiveCount() + ",任务数量：" + threadPoolExecutor.getTaskCount() + "，完成任务数量：" + threadPoolExecutor.getCompletedTaskCount());
+                    try {
+                        Thread.sleep(5000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("执行结束。");
+                }
+            });
+        }
+
+    }
+
+
+}
