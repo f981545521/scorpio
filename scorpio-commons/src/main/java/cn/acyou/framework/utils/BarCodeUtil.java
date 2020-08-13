e8846e6d9e0d9cd908810bcc4151ab749d272d91
youfang <981545521@qq.com>
2020-08-13 23:45:04 +0800

    Zxing

---
+++ scorpio-commons/src/main/java/cn/acyou/framework/utils/BarCodeUtil.java
@@ -0,0 +1,142 @@
+package cn.acyou.framework.utils;
+
+import com.google.zxing.*;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.common.HybridBinarizer;
+import com.google.zxing.pdf417.PDF417Writer;
+import com.google.zxing.qrcode.QRCodeReader;
+import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.util.Hashtable;
+
+/**
+ * @author youfang
+ * @version [1.0.0, 2020-8-13 下午 11:06]
+ **/
+public class BarCodeUtil {
+    private static final int BLACK = 0xff000000;
+    private static final int WHITE = 0xFFFFFFFF;
+
+    /**
+     * 生成包含字符串信息的二维码图片
+     *
+     * @param outputStream 文件输出流路径
+     * @param content      二维码携带信息
+     * @param qrCodeSize   二维码图片大小
+     * @param imageFormat  二维码的格式
+     * @throws WriterException
+     * @throws IOException
+     */
+    public static boolean createQrCode(OutputStream outputStream, String content, int qrCodeSize, String imageFormat)
+            throws WriterException, IOException {
+        //设置二维码纠错级别ＭＡＰ
+        Hashtable<EncodeHintType, Object> hintMap = new Hashtable<>();
+        hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L); // 矫错级别
+        hintMap.put(EncodeHintType.MARGIN, 1);
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        //创建比特矩阵(位矩阵)的QR码编码的字符串
+        BitMatrix byteMatrix = qrCodeWriter.encode(content, BarcodeFormat.QR_CODE, qrCodeSize, qrCodeSize, hintMap);
+        // 使BufferedImage勾画QRCode  (matrixWidth 是行二维码像素点)
+        int matrixWidth = byteMatrix.getWidth();
+        BufferedImage image = new BufferedImage(matrixWidth - 200, matrixWidth - 200, BufferedImage.TYPE_INT_RGB);
+        image.createGraphics();
+        Graphics2D graphics = (Graphics2D) image.getGraphics();
+        graphics.setColor(Color.WHITE);
+        graphics.fillRect(0, 0, matrixWidth, matrixWidth);
+        // 使用比特矩阵画并保存图像
+        graphics.setColor(Color.BLACK);
+        for (int i = 0; i < matrixWidth; i++) {
+            for (int j = 0; j < matrixWidth; j++) {
+                if (byteMatrix.get(i, j)) {
+                    graphics.fillRect(i - 100, j - 100, 1, 1);
+                }
+            }
+        }
+        return ImageIO.write(image, imageFormat, outputStream);
+    }
+    /**
+     * 生成包含字符串信息的二维码图片
+     *
+     * @param outputStream 文件输出流路径
+     * @param content      二维码携带信息
+     * @param imageFormat  二维码的格式
+     * @throws WriterException
+     * @throws IOException
+     */
+    public static boolean createPDF417Code(OutputStream outputStream, String content, String imageFormat)
+            throws WriterException, IOException {
+        PDF417Writer pdf417Writer = new PDF417Writer();
+        BitMatrix bitMatrix = pdf417Writer.encode(content, BarcodeFormat.PDF_417, 100, 50);
+        BufferedImage image = toBufferedImage(bitMatrix);
+        return ImageIO.write(image, imageFormat, outputStream);
+    }
+
+    private static BufferedImage toBufferedImage(BitMatrix matrix) {
+        int width = matrix.getWidth();
+        int height = matrix.getHeight();
+        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                image.setRGB(x, y, matrix.get(x, y) ? BLACK : WHITE);
+            }
+        }
+        return image;
+    }
+
+    /**
+     * 读二维码并输出携带的信息
+     */
+    public static void readQrCode(InputStream inputStream)
+            throws IOException {
+        //从输入流中获取字符串信息
+        BufferedImage image = ImageIO.read(inputStream);
+        //将图像转换为二进制位图源
+        LuminanceSource source = new BufferedImageLuminanceSource(image);
+        BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+        QRCodeReader reader = new QRCodeReader();
+        Result result = null;
+        try {
+            result = reader.decode(bitmap);
+        } catch (ReaderException e) {
+            e.printStackTrace();
+        }
+        System.out.println(result.getText());
+    }
+
+    private static BufferedImage createImage(String content) throws WriterException {
+        Hashtable hints = new Hashtable();
+        hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
+        hints.put(EncodeHintType.CHARACTER_SET, "utf-8");
+        hints.put(EncodeHintType.MARGIN, 1);
+        BitMatrix bitMatrix = new MultiFormatWriter().encode(content,
+                BarcodeFormat.QR_CODE, 300, 300, hints);
+        int width = bitMatrix.getWidth();
+        int height = bitMatrix.getHeight();
+        BufferedImage image = new BufferedImage(width, height,
+                BufferedImage.TYPE_INT_RGB);
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                image.setRGB(x, y, bitMatrix.get(x, y) ? 0xFF000000
+                        : 0xFFFFFFFF);
+            }
+        }
+        return image;
+    }
+
+    public static void main(String[] args) throws IOException, WriterException {
+
+        //createQrCode(new FileOutputStream(new File("d:\\qrcode2.jpg")), "111", 300, "JPEG");
+        //readQrCode(new FileInputStream(new File("d:\\qrcode.jpg")));
+
+        //BufferedImage image = createImage("110");
+        //ImageIO.write(image, "PNG", new FileOutputStream(new File("d:\\qrcode211.jpg")));
+
+        createPDF417Code(new FileOutputStream(new File("d:\\temp\\pdf417.jpg")), "1111111111111AAA1", "PNG");
+    }
+}
