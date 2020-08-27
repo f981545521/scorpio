ac0d39bc90982a189700eb62b6fb31814a897344
youfang <981545521@qq.com>
2020-08-27 18:19:14 +0800

    update sql

--- document/notes/spring cloud/prepare/prepare.sql
+++ document/notes/spring cloud/prepare/prepare.sql
@@ -8,6 +8,7 @@ CREATE TABLE `t_order` (
    `status` int(3) NOT NULL DEFAULT '0',
    PRIMARY KEY (`order_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=100 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='Order';
+
 CREATE TABLE `undo_log` (
     `id` bigint(20) NOT NULL AUTO_INCREMENT,
     `branch_id` bigint(20) NOT NULL,
@@ -20,7 +21,7 @@ CREATE TABLE `undo_log` (
     `ext` varchar(100) DEFAULT NULL,
     PRIMARY KEY (`id`),
     UNIQUE KEY `ux_undo_log` (`xid`,`branch_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 Comment = 'seata日志表';
 
 
 
@@ -35,6 +36,9 @@ CREATE TABLE `t_product` (
     `stock_number` int(3) NOT NULL DEFAULT '0',
     PRIMARY KEY (`product_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=100 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='Product';
+INSERT INTO `aquarius_product`.`t_product`(`product_id`, `product_name`, `status`, `stock_number`) VALUES (1, '笔记本', 0, 1000);
+
+
 CREATE TABLE `undo_log` (
     `id` bigint(20) NOT NULL AUTO_INCREMENT,
     `branch_id` bigint(20) NOT NULL,
@@ -47,4 +51,4 @@ CREATE TABLE `undo_log` (
     `ext` varchar(100) DEFAULT NULL,
     PRIMARY KEY (`id`),
     UNIQUE KEY `ux_undo_log` (`xid`,`branch_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 Comment = 'seata日志表';
