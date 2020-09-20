46238601ae79b89d9b1b994191c59376a23167d1
youfang <youfang@acyou.cn>
2020-09-20 19:43:26 +0800

    MySQL章节

---
+++ document/notes/mysql/#MySQL的规范.md
@@ -0,0 +1,20 @@
+---
+title: 标题
+date: 2020-09-20 19:24:59
+tags: 
+---
+## MySQL的规范
+
+>
+> 阿里巴巴开发规范的**SQL 语句**章节中
+> 1. 【强制】不要使用 count(列名)或 count(常量)来替代 count(*)，count(*)是 SQL92 定义的标
+> 准统计行数的语法，跟数据库无关，跟 NULL 和非 NULL 无关。
+> 说明：count(*)会统计值为 NULL 的行，而 count(列名)不会统计此列为 NULL 值的行。
+> 2. 【强制】count(distinct col) 计算该列除 NULL 之外的不重复行数，注意 count(distinct col1,
+> col2) 如果其中一列全为 NULL，那么即使另一列有不同的值，也返回为 0。
+>
+>
+
+参照[实战说明](1.测试MySQL的Like查询效率.md)后得到结论：以后还是用count(*)
+
+
---
+++ document/notes/mysql/1.测试MySQL的Like查询效率.sql
@@ -0,0 +1,52 @@
+-- 准备数据
+-- 1.1 创建表
+CREATE TABLE `product` (
+    `id` int(10) NOT NULL AUTO_INCREMENT COMMENT '主键,不为空',
+    `merchant_id` int(11) DEFAULT NULL COMMENT '商家id',
+    `product_name` varchar(128) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '商品名称',
+    `product_title` varchar(128) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '商品标题',
+    `classify_1` int(10) DEFAULT NULL COMMENT '1级分类',
+    `classify_2` int(10) DEFAULT NULL COMMENT '2级分类',
+    `classify_3` int(10) DEFAULT NULL COMMENT '3级分类',
+    `brand_id` int(10) DEFAULT NULL COMMENT '品牌',
+    `product_amount` decimal(10,0) DEFAULT NULL COMMENT '商品金额',
+    `sales_amount` decimal(10,0) DEFAULT NULL COMMENT '销售金额',
+    `product_status` int(10) DEFAULT NULL COMMENT '商品状态,1删除，0正常',
+    `flag` int(10) DEFAULT NULL COMMENT '标记',
+    `freight` decimal(10,2) DEFAULT NULL COMMENT '运费',
+    `sku_info` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '规格说明',
+    `remarks` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '售后说明',
+    `pic_url` varchar(500) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '预览图片地址',
+    `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+    `update_time` datetime DEFAULT NULL COMMENT '修改时间',
+    `user_id` int(10) DEFAULT NULL COMMENT '操作人',
+    `user_ip` varchar(128) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '操作人ip',
+    `product_name_query` varchar(255) GENERATED ALWAYS AS (locate('8',`product_name`)) STORED,
+    PRIMARY KEY (`id`),
+    FULLTEXT KEY `index_name` (`product_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='商品信息表';
+
+-- 1.2 创建存储过程
+CREATE DEFINER=`root`@`localhost` PROCEDURE `insert_product3`(IN total BIGINT)
+BEGIN
+DECLARE start_key BIGINT DEFAULT 0;
+DECLARE max_id BIGINT DEFAULT 0;
+SELECT COUNT(1) into max_id from product;
+WHILE start_key<total
+DO
+SET max_id=max_id+1;
+INSERT INTO product(product_title,product_name,remarks,merchant_id,create_time,update_time,classify_1,classify_2,classify_3,brand_id,user_id,user_ip,freight,pic_url,sales_amount,product_amount,product_status)
+VALUES(CONCAT('商品_iphone8_title_test_',max_id),CONCAT('商品_iphone8_name_test_',max_id),CONCAT('商品_iphone8_remrk_test_',max_id),max_id,NOW(),NOW(),1,1,1,1,1000,'192.168.18.100',5000,'http://www.baidu.com',5000,5000,0);
+SET start_key=start_key+1;
+COMMIT;
+END WHILE;
+END
+
+-- 1.3 调用存储过程
+call insert_product3(1000);
+
+> 数字是要增加记录的数量
+
+
+
+
