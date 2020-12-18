a20c3d1ee101c159f57d95a148d7b957daa05c7d
youfang <youfang@acyou.cn>
2020-12-18 15:54:59 +0800

    打印问题

--- scorpio-web/src/test/java/cn/acyou/scorpio/test/informal/testutil/PrinterTest.java
+++ scorpio-web/src/test/java/cn/acyou/scorpio/test/informal/testutil/PrinterTest.java
@@ -12,7 +12,12 @@ public class PrinterTest {
 
     public static void main(String[] args) {
         PrintService printService = PrintServiceLookup.lookupDefaultPrintService();
-        String strData = PrinterUtil.formatGoodsCode("身份识别", "", "", "", "7ab1315b640240");
+        String strData = PrinterUtil.formatGoodsCode(
+                "眼科手术钩-物品条码",
+                "100/400-1",
+                "GK888T",
+                "",
+                "353454612342");
         DocPrintJob printJob = printService.createPrintJob();
         byte[] by = PrinterUtil.str2bytes(strData);
         DocFlavor flavor = DocFlavor.BYTE_ARRAY.AUTOSENSE;
